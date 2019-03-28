0aa18fc1df30498b7ece81db900e2c30304f3610
Maksim Sukhotski <suhockii@gmail.com>
2019-03-28 18:11:04 +0300

    Screen with subcategories

---
+++ app/src/main/java/kt/school/starlord/ui/subcategories/SubcategoriesFragment.kt
@@ -0,0 +1,57 @@
+package kt.school.starlord.ui.subcategories
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import kotlinx.android.synthetic.main.fragment_categories.*
+import kt.school.starlord.R
+import kt.school.starlord.entity.Category
+import kt.school.starlord.ui.global.CategoryAdapterDelegate
+import org.jetbrains.anko.support.v4.toast
+
+class SubcategoriesFragment: Fragment() {
+
+    private val adapter by lazy { SubcategoriesAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            val safeArgs = SubcategoriesFragmentArgs.fromBundle(it)
+            adapter.setData(listOf(Category(safeArgs.categoryName, 0)))
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_subcategories, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = this@SubcategoriesFragment.adapter
+        }
+    }
+
+    private inner class SubcategoriesAdapter : ListDelegationAdapter<MutableList<Any>>() {
+        init {
+            items = mutableListOf()
+            delegatesManager.addDelegate(CategoryAdapterDelegate { toast("$it") })
+        }
+
+        fun setData(data: List<Any>) {
+            items.clear()
+            items.addAll(data)
+
+            notifyDataSetChanged()
+        }
+    }
+}
