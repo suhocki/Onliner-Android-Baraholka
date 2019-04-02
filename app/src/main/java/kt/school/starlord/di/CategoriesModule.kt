0c3389e76529919ed82880fd41cabb7912ec8a9b
pcfaktor <pcfaktor@gmail.com>
2019-04-02 23:30:34 +0300

    Add page loader and page parser

--- app/src/main/java/kt/school/starlord/di/CategoriesModule.kt
+++ app/src/main/java/kt/school/starlord/di/CategoriesModule.kt
@@ -1,14 +1,14 @@
 package kt.school.starlord.di
 
 import kt.school.starlord.domain.CategoriesRepository
-import kt.school.starlord.model.MockRepository
+import kt.school.starlord.model.NetworkRepository
 import kt.school.starlord.ui.categories.CategoriesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val categoriesModule = module {
 
-    single<CategoriesRepository> { MockRepository() }
+    single<CategoriesRepository> { NetworkRepository() }
 
     viewModel { CategoriesViewModel(get()) }
 }
