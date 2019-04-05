d11d12811812eed8d310f9dcaa2145330d1ad7c6
AMartsinkevich <saint_wolf@tut.by>
2019-04-05 10:44:20 +0300

    JSOUP parsing

--- app/src/main/java/kt/school/starlord/stw/stw.kt
+++ app/src/main/java/kt/school/starlord/stw/stw.kt
@@ -5,8 +5,6 @@ import org.jsoup.Jsoup
 
 import java.io.IOException
 
-//import java.awt.SystemColor.text
-
 object JSoupLinksEx {
 
     @Throws(IOException::class)
@@ -17,15 +15,12 @@ object JSoupLinksEx {
 
         val document = Jsoup.connect(url).get()
         val links = document.select("div.cm-onecat li a")
-
+        val nums = document.select("div.cm-onecat sup")
+        var index = 0
         val list = mutableListOf<Subcategory>()
-
         for (link in links) {
-
-//            System.out.println("http://baraholka.onliner.by" + link.attr("href").substring(1))
-//            System.out.println("text : " + link.text())
-            list += listOf(Subcategory("http://baraholka.onliner.by" + link.attr("href").substring(1),null ,link.text()))
-//            println(list)
+            list += listOf(Subcategory("http://baraholka.onliner.by" + link.attr("href").substring(1),nums[index].text().toInt() ,link.text()))
+            index++
         }
     }
 }
