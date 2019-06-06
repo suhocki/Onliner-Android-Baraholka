ff075cf9d6a7b35fc5a0f14a8c323b4c627f4ab6
Maksim Sukhotski <suhockii@gmail.com>
2019-06-06 18:08:46 +0300

    Add auto-chmod scripts

--- scripts/telegram_notify_on_google_play_publish.sh
+++ scripts/telegram_notify_on_google_play_publish.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+release_notes_file="app/src/main/play/release-notes/ru-RU/default.txt"
+current_build_version=$(./scripts/versionizer/versionizer.sh name)
 BOT_URL="https://api.telegram.org/bot${telegram_bot_token}/sendMessage"
-
 PARSE_MODE="Markdown"
 
 send_msg () {
@@ -12,6 +13,6 @@ send_msg () {
 send_msg "
 New release is available on [Google Play](https://play.google.com/store/apps/details?id=kt.school.starlord)!
 
-Whats new:
-\`• ${GITHUB_ISSUE}\`
+Whats new in v${current_build_version}:
+\`$(cat ${release_notes_file})\`
 "
