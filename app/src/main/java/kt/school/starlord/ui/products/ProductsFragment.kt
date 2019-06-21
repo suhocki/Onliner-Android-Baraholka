a1268a8f68afdb12463f40fa81cddf410f1ca5f8
Makaronina22 <efr.romanyuk@mail.ru>
2019-06-21 17:02:13 +0300

    Fix PR

--- app/src/main/java/kt/school/starlord/ui/products/ProductsFragment.kt
+++ app/src/main/java/kt/school/starlord/ui/products/ProductsFragment.kt
@@ -5,51 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
-import kotlinx.android.synthetic.main.fragment_categories.*
 import kt.school.starlord.R
-import kt.school.starlord.ui.categories.CategoryAdapterDelegate
-import org.jetbrains.anko.support.v4.toast
 
 /**
  * Contains a recycler that is filled by products.
  *
  * Clicking on an item takes you to a product fragment.
  */
-
 class ProductsFragment : Fragment() {
 
-    private val adapter by lazy { ProductsAdapter() }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_products, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        with(recyclerView) {
-            layoutManager = LinearLayoutManager(context)
-            setHasFixedSize(true)
-            adapter = this@ProductsFragment.adapter
-        }
-    }
-
-    private inner class ProductsAdapter : ListDelegationAdapter<MutableList<Any>>() {
-        init {
-            items = mutableListOf()
-            delegatesManager.addDelegate(CategoryAdapterDelegate { toast("$it") })
-        }
-
-        fun setData(data: List<Any>) {
-            items.clear()
-            items.addAll(data)
-
-            notifyDataSetChanged()
-        }
-    }
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_products, container, false)
 }
