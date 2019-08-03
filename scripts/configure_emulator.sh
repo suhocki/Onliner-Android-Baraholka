a63c56bae60831838f567261bd11fb7eea0998c4
Maksim Sukhotski <maksim.sukhotski@consultant.turkcell.com.tr>
2019-08-03 19:26:25 +0300

    Fix waiting for emulator

--- scripts/configure_emulator.sh
+++ scripts/configure_emulator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-adb wait-for-device
+android-wait-for-emulator
 adb shell settings put global window_animation_scale 0 &
 adb shell settings put global transition_animation_scale 0 &
 adb shell settings put global animator_duration_scale 0 &
