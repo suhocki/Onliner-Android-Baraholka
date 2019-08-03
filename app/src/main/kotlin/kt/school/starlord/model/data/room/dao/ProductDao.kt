47b328a4cd712330fe8e0fb267d7235cf4b6f8af
Maksim Sukhotski <maksim.sukhotski@consultant.turkcell.com.tr>
2019-08-04 02:00:34 +0300

    Remove unused code

--- app/src/main/kotlin/kt/school/starlord/model/data/room/dao/ProductDao.kt
+++ app/src/main/kotlin/kt/school/starlord/model/data/room/dao/ProductDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Transaction
 import kt.school.starlord.model.data.room.entity.RoomProduct
 
 /**
@@ -19,18 +18,6 @@ abstract class ProductDao {
      */
     @Query("SELECT * FROM Products WHERE subcategoryName=:subcategoryName")
     abstract fun getProducts(subcategoryName: String): LiveData<List<RoomProduct>>
-
-    /**
-     * Replaces old products with a new ones. Previous products with provided subcategoryName will be dropped.
-     *
-     * @param subcategoryName name of selected subcategory.
-     * @param products items that will be saved in database.
-     */
-    @Transaction
-    open suspend fun replaceAll(subcategoryName: String, products: List<RoomProduct>) {
-        deleteAll(subcategoryName)
-        putProducts(products)
-    }
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     protected abstract suspend fun putProducts(products: List<RoomProduct>)
