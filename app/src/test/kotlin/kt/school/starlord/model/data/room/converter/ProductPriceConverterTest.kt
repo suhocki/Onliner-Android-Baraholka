df7ec26127966f0fd9e41ca90b85dac1ae68446a
Maksim Sukhotski <maksim.sukhotski@consultant.turkcell.com.tr>
2019-08-04 10:00:13 +0300

    Add unit tests on Room converters

--- app/src/test/kotlin/kt/school/starlord/model/data/room/converter/ProductPriceConverterTest.kt
+++ app/src/test/kotlin/kt/school/starlord/model/data/room/converter/ProductPriceConverterTest.kt
@@ -5,20 +5,20 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import io.mockk.every
 import io.mockk.mockk
-import kt.school.starlord.entity.product.ProductOwner
+import kt.school.starlord.entity.product.ProductPrice
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.mock.declare
-import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers
 
 @RunWith(AndroidJUnit4::class)
-class ProductOwnerConverterTest : AutoCloseKoinTest() {
+class ProductPriceConverterTest : AutoCloseKoinTest() {
 
     private val moshi: Moshi = mockk()
-    private val adapter = mockk<JsonAdapter<ProductOwner>>()
-    private val converter by lazy { ProductOwnerConverter() }
+    private val adapter = mockk<JsonAdapter<ProductPrice>>()
+    private val converter by lazy { ProductPriceConverter() }
 
     @Before
     fun setUp() {
@@ -26,34 +26,34 @@ class ProductOwnerConverterTest : AutoCloseKoinTest() {
             single { moshi }
         }
 
-        every { moshi.adapter(ProductOwner::class.java) } returns adapter
+        every { moshi.adapter(ProductPrice::class.java) } returns adapter
     }
 
     @Test
     fun `from Room type`() {
         // Given
-        val json = anyString()
-        val productOwner: ProductOwner = mockk()
+        val json = ArgumentMatchers.anyString()
+        val productPrice: ProductPrice = mockk()
 
-        every { adapter.fromJson(json) } returns productOwner
+        every { adapter.fromJson(json) } returns productPrice
 
         // When
         val result = converter.fromRoomType(json)
 
         // Then
-        assert(result == productOwner)
+        assert(result == productPrice)
     }
 
     @Test
     fun `to Room type`() {
         // Given
-        val json = anyString()
-        val productOwner: ProductOwner = mockk()
+        val json = ArgumentMatchers.anyString()
+        val productPrice: ProductPrice = mockk()
 
-        every { adapter.toJson(productOwner) } returns json
+        every { adapter.toJson(productPrice) } returns json
 
         // When
-        val result = converter.toRoomType(productOwner)
+        val result = converter.toRoomType(productPrice)
 
         // Then
         assert(result == json)
---
+++ app/src/test/kotlin/kt/school/starlord/model/data/room/converter/ProductTypeConverterTest.kt
@@ -0,0 +1,32 @@
+package kt.school.starlord.model.data.room.converter
+
+import kt.school.starlord.entity.product.ProductType
+import org.junit.Test
+
+class ProductTypeConverterTest {
+    private val converter = ProductTypeConverter()
+
+    @Test
+    fun `to Room type`() {
+        // Given
+        val productType = ProductType.RENT
+
+        // When
+        val answer = converter.toRoomType(productType)
+
+        // Then
+        assert(ProductType.valueOf(answer) == productType)
+    }
+
+    @Test
+    fun `from Room type`() {
+        // Given
+        val string = ProductType.RENT.name
+
+        // When
+        val answer = converter.fromRoomType(string)
+
+        // Then
+        assert(answer == ProductType.RENT)
+    }
+}
